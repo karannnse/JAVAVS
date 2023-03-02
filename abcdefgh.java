0def719e14147a848a1899e683bfe807e2368c9e
karannnse <karankkataria.31@gmail.com>
2023-03-02 18:55:06 +0530

    looppatterns

---
+++ abcdefgh.java
@@ -0,0 +1,137 @@
+public class abcdefgh {
+    
+    public static void main(String[] args) {
+        int a=16;
+        for(int i=0;i<a ; i++)
+        {
+            for(int j=0;j<a ;j++)
+            {
+                if (i==0 && j>0 && j<(a-1)/2 || j==0 && i>0 || i==(a-1)/2 && j<=(a-1)/2 || j==(a-1)/2 && i>0) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+                
+                
+
+
+      /*    int b=16;
+        for(int i=0;i<b ; i++)
+        {
+            for(int j=0;j<b ;j++)
+            {
+                if (i==0 && j<(b-1)/2 || j==0 || i==b-1 && j<(b-1)/2 ||j==(b-1)/2 && i>0 && i<b-1 || i==(b-1)/2 && j<(b-1)/2 ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+        
+        int c=16;
+        for(int i=0;i<c ; i++)
+        {
+            for(int j=0;j<c ;j++)
+            {
+                if (i==0 && j<(c-1)/2 && j>0 || j==0 && i>0 && i<(c-1) || i==(c-1) && j<=(c-1)/2  ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+
+        int d=16;
+        for(int i=0;i<d ; i++)
+        {
+            for(int j=0;j<d ;j++)
+            {
+                if (i==0 && j<(d-1)/2 || j==0 || i==(d-1) && j<(d-1)/2 || j==(d-1)/2 && i>0 && i<(d-1) ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+       int e=16;
+        for(int i=0;i<e; i++)
+        {
+            for(int j=0;j<e ;j++)
+            {
+                if (i==0 && j<(e-1)/2 || j==0 || i==(e-1) && j<(e-1)/2 || i==(e-1)/2 &&  j<=(e-1)/2 ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+    
+    
+        int f=16;
+        for(int n=0;n<f ; n++)
+        {
+            for(int j=0;j<f ;j++)
+            {
+                if (n==0 && j<(f-1)/2 || j==0 || n==(f-1)/2 && j<=(f-1)/2 ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+        int h=16;
+        for(int k=0;k<h ; k++)
+        {
+            for(int j=0;j<h ;j++)
+            {
+                if (  j==0 || k==(h-1)/2
+                  || j==(h-1) ) 
+                {
+                    System.out.print("*");
+
+                } else
+                 {
+                System.out.print(" ");
+
+                }
+            }
+            System.out.println();
+        }
+    }*/
+
+}
+}
+        
+        
+        
